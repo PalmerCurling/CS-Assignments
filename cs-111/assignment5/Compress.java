c408d26bcff673b5ed98d6ead84f3ce7312886a5
Eric Meadow <palmercurling@android.net>
2013-03-06 12:39:22 -0500

    need to change to IO, but works

--- cs-111/assignment5/Compress.java
+++ cs-111/assignment5/Compress.java
@@ -1,19 +1,30 @@
 public class Compress{
 	public static void main(String[] args){
 		String s = IO.readString();
-		int count = 0;
+		int count = 1;
 		for(int i = 0; i < s.length(); i++){
+			if(i+1 == s.length()){
+				if(count == 1){
+					System.out.print(s.charAt(i));
+				}else{
+					System.out.print(count);
+					System.out.print(s.charAt(i));
+					count = 1;
+				}
+					break;
+			}
 			if(s.charAt(i) == s.charAt(i+1)){
 				count++;
 			}else{
 				if(count == 1){
 					System.out.print(s.charAt(i));
-					count = 0;
 				}else{
-					System.out.print(count+s.charAt(i));
-					count = 0;
+					System.out.print(count);
+					System.out.print(s.charAt(i));
+					count = 1;
 				}
 			}
 		}
+		System.out.println(" ");
 	}
 }
