b9a43e9611a12fb9fa4df5dff17b69e45197bdd3
Eric Meadow <palmercurling@android.net>
2013-03-15 00:40:40 -0400

    first frame and sprites added

--- cs-111/assignment6/MyMovie.java
+++ cs-111/assignment6/MyMovie.java
@@ -11,13 +11,40 @@ public class MyMovie{
 		System.out.println("kurumu2.jpg");
 		System.out.println("kurumu3.jpg");
 		System.out.println("kurumu4.jpg");
+		System.out.println("Credit to agrs700 for the sprites!");
 		System.out.println("Now loading: Rosario+Vampire: Java Battle");
 
 		Sprite moka1 = new Sprite("moka1.jpg");
 		Sprite moka2 = new Sprite("moka2.jpg");
 		Sprite moka3 = new Sprite("moka3.jpg");
-		Sprite tsukune = new Sprite("tsukune.jpg");
 		Sprite moka4 = new Sprite("moka4.jpg");
+		Sprite tsukune = new Sprite("tsukune.jpg");
 		Sprite kurumu1 = new Sprite("kurumu1.jpg");
+		Sprite kurumu2 = new Sprite("kurumu2.jpg");
+		Sprite kurumu3 = new Sprite("kurumu3.jpg");
+		Sprite kurumu4 = new Sprite("kurumu4.jpg");
+
+		moka1.setSize(37,79);
+		moka2.setSize(38,81);
+		moka3.setSize(55,65);
+		moka4.setSize(85,64);
+		tsukune.setSize(37,76);
+		kurumu1.setSize(25,79);
+		kurumu2.setSize(87,85);
+		kurumu3.setSize(94,74);
+		kurumu4.setSize(70,75);
+
+		Animation fight = new Animation(800,450);
+		fight.setFrameRate(1);
+		fight.setBackgroundImage("background.jpg");
+
+		moka1.setPosition(0,0);
+		kurumu1.setPosition((800-76),0);
+		tsukune.setPosition((40),0);
+		fight.addSprite(moka1);
+		fight.addSprite(kurumu1);
+		fight.addSprite(tsukune);
+		fight.frameFinished();
+
 	}
 }
