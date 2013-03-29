7954f23918c08371930a204e7f209096547ff52f
Eric Meadow <palmercurling01069@gmail.com>
2013-03-29 15:46:41 -0400

    working matrixops

--- cs-111/assignment8/MatrixOps.java
+++ cs-111/assignment8/MatrixOps.java
@@ -1,24 +1,24 @@
 public class MatrixOps{
 	public static double[][] multiply(double[][] matrix1, double[][] matrix2){
 		
-		double[][] sum;
-		int row1 = matrix1.length;
-		int column1 = matrix1[0].length;
-		int row2 = matrix2.length;
-		int column2 = matrix2[0].length; 
-		
-		if(column1 != row2){
+		if(matrix1[0].length != matrix2.length){
 			return null;
 		}else{
-			sum = new double[row1][column2];
-			for(int i = 0; i < row1; i++){
-				for(int j = 0; i < column2; j++){
-					for(int k = 0; k < column1; k++){
-						sum[i][j] += matrix1[i][k] * matrix2[k][j];
+			
+			int n = matrix1[0].length;
+			int m = matrix1.length;
+			int p = matrix2[0].length;
+
+			double ans[][] = new double[m][p];
+
+			for(int i = 0; i < m; i++){
+				for(int j = 0; j < p; j++){
+					for(int k = 0; k < n; k++){
+						ans[i][j] += matrix1[i][k] * matrix2[k][j];
 					}
 				}
 			}
-			return sum;
+			return ans;
 		}
 	}
 }
