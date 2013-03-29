29e4fb1d2b4dca21d996ebfdbeca0161171398b6
Eric Meadow <palmercurling01069@gmail.com>
2013-03-29 12:45:40 -0400

    improved MatrixOps.java; still not functioning

--- cs-111/assignment8/MatrixOps.java
+++ cs-111/assignment8/MatrixOps.java
@@ -1,23 +1,23 @@
 public class MatrixOps{
 	public static double[][] multiply(double[][] matrix1, double[][] matrix2){
+		
 		double[][] sum;
-		int row = matrix2.length;
-		int column = matrix1[0].length;
-
-		if(column != row){
+		int row1 = matrix1.length;
+		int column1 = matrix1[0].length;
+		int row2 = matrix2.length;
+		int column2 = matrix2[0].length; 
+		
+		if(column1 != row2){
 			return null;
 		}else{
-			sum = new double[row][column];
-			for(int i = 0; i < row; i++){
-				for(int j = 0; i < column; j++){
-					sum[i][j] = 0;
-					for(int k = 0; k < row; k++){
-						sum[i][k] += (matrix1[i][k] * matrix2[k][j]);
+			sum = new double[row1][column2];
+			for(int i = 0; i < row1; i++){
+				for(int j = 0; i < column2; j++){
+					for(int k = 0; k < column1; k++){
+						sum[i][j] += matrix1[i][k] * matrix2[k][j];
 					}
 				}
-
 			}
-
 			return sum;
 		}
 	}
