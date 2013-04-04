7a74ca761464439d8354ec846b544cb88f2bb8d2
Eric Meadow <palmercurling@android.net>
2013-04-04 01:23:10 -0400

    dammit roberto dont modify my code just message me

--- projects/minesweeper/RevealCell.java
+++ projects/minesweeper/RevealCell.java
@@ -1,7 +1,5 @@
 public class RevealCell{
-	// so yeah file named reveal cell...
 	public static char[][] revealCell(char[][]staticBoard, char[][] dynamicBoard, int[] choice){
-		// but wtf does this method do??
 		dynamicBoard[choice[0]][choice[1]] = staticBoard[choice[0]][choice[1]];
 		return dynamicBoard;
 	}
