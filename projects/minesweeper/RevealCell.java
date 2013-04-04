cd1f52718a99ecce68fa97e161bc7bd07b97d89f
Roberto Cruz <RobertoECruz12@gmail.com>
2013-04-04 01:56:53 -0300

    Confused about this code
    Get back to me on fb or w/e

--- projects/minesweeper/RevealCell.java
+++ projects/minesweeper/RevealCell.java
@@ -1,5 +1,7 @@
 public class RevealCell{
+	// so yeah file named reveal cell...
 	public static char[][] revealCell(char[][]staticBoard, char[][] dynamicBoard, int[] choice){
+		// but wtf does this method do??
 		dynamicBoard[choice[0]][choice[1]] = staticBoard[choice[0]][choice[1]];
 		return dynamicBoard;
 	}
